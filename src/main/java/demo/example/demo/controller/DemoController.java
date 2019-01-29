cd5d2a7b6b29d29c4cacf5f44d091acb8b87fce6
takeshi harada <t.harada@gxp.co.jp>
2019-01-29 16:53:14 +0900

    SPF-0001 警告の発生するケースを確認するコードを追加

--- src/main/java/demo/example/demo/controller/DemoController.java
+++ src/main/java/demo/example/demo/controller/DemoController.java
@@ -1,14 +1,14 @@
 package demo.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import demo.example.demo.dto.User;
 import demo.example.demo.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * デモ用APIコントローラー.
@@ -29,6 +29,10 @@ public class DemoController {
     public List<User> getUsers() {
 
         // ユーザ情報が存在しない場合、空のユーザ情報を返す
+        if (!userService.isUser("test.user")) {
+            return new ArrayList<>();
+        }
+
         if (!userService.isUser("test.user")) {
             return new ArrayList<>();
         }
